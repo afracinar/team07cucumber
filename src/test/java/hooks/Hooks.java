c3dec77590c5a4a9a96bc4c0c6b6c197600aecac
Afra Cinar <afracinaarr@gmail.com>
2023-03-08 13:18:26 +0300

    v1

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,25 @@
+package hooks;
+
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+public class Hooks {
+
+   @Before
+   public void before(){
+       System.out.println("Before Methodu her scenariodan veya scenario outlinedan önce çalışır");
+   }
+
+    @After
+    public void hooks(Scenario scenario){
+          if (scenario.isFailed()){
+                 byte [] failedScreenshot =((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+
+          }
+    }
+}
---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,26 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty", //raporların daha okunaklı olması için eklenir
+                "html:target/default-cucumber-reports.html", //html raporu browser da açılır.sağ tık yapıp open in dememiz lazım
+                "json:target/json-reports/cucumber.json", //json raporu üzerine çift tıklayıp açılır
+                "junit:target/xml-report/cucumber.xlm", //junit raporu üzerine çift tıklayıp açılır
+                "rerun:target/failed_scenarios.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome = true, //concole'daki bilgilerin okunaklı olmasını sağlar
+        features = "/.src/test/resources/features",//features folder path
+        glue = {"stepdefinitions" ,"hooks"},//stepdefinitions path
+        tags = "",
+        dryRun = false
+
+)
+
+public class Runner {
+}
